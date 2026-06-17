b2064e84563c0d17b9c7cad01917ec7594ac403c
gbatistaa <gbatistadev@gmail.com>
2026-06-17 10:13:32 -0300

    feat: initialize Spring Boot backend project with Maven and required starters

---
+++ backend/src/main/java/com/ses/pizzamanagement/PizzamanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ses.pizzamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzamanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzamanagementApplication.class, args);
+	}
+
+}
